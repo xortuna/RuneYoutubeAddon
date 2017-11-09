5aecc983929f224835b6eac0b88c20cf641a0252
xortuna <jacdye@gmail.com>
2017-11-09 20:44:50 +0000

    uninstall youtube

--- usr/local/bin/uninstall_RuneYoutube.sh
+++ usr/local/bin/uninstall_RuneYoutube.sh
@@ -11,27 +11,24 @@ uninstallstart $@
 
 # start custom script ------------------------------------------------------------------------------>>>
 
-echo -e "$bar Remove youtube-dl ..."
+echo -e "$bar Remove youtube-dl..."
 pacman -R --noconfirm youtube-dl
 
-echo -e "$bar Remove php script ..."
+echo -e "$bar Remove web files..."
 rm -v /srv/http/youtube.php
-echo -e "$bar Remove shell youtube script ..."
+echo -e "$bar Remove shell scripts..."
 rm -v /usr/local/bin/tube
-echo -e "$bar Remove shell playlist script ..."
 rm -v /usr/local/bin/tubeplaylist
-echo -e "$bar Restore files ..."
+echo -e "$bar Patch files..."
 file=/srv/http/app/templates/playback.php
 echo $file
 sed -i $'/<!-- RUNE_YOUTUBE_MOD -->/,/<!-- END_RUNE_YOUTUBE_MOD -->/ d' $file
-
-echo -e "$bar Modify files ..."
 file=/srv/http/assets/js/runeui.js
 echo $file
 	sed -i $'/\/\/RUNE_YOUTUBE_MOD/,/\/\/END_RUNE_YOUTUBE_MOD/ d' $file
 
 
-echo -e "$bar Removing youtube directory ..."
+echo -e "$bar Removing youtube directory.."
 rm -r /mnt/MPD/LocalStorage/Youtube
 # end custom script --------------------------------------------------------------------------------<<<
 
